bba9889726b65a5d61566ba7944b6d52c7f9f9d7
John <johnd.wilson9@gmail.com>
2018-02-20 13:23:12 -0500

    Cleaned up main function

--- matchmaker.py
+++ matchmaker.py
@@ -3,50 +3,9 @@
 import os
 import csv
 import argparse
-import calendar
+from class_module import Pickup, Recipient
 import pandas as pd
-from dateutil import parser
 from geopy.distance import vincenty
-
-class Pickup:
-    def __init__(self, d):
-        self.Matches = []
-
-        for key, value in d.items():
-            setattr(self, key, value)
-
-        # Change PickupAt from type str to type datetime object
-        self.PickupAt = parser.parse(self.PickupAt)
-
-    def full_name(self):
-        return self.FirstName + " " + self.LastName
-
-class Recipient:
-    def __init__(self, d):
-        # Keep track of hours of operation in dictionary
-        # e.g. {"Sunday": 44536, "Monday": 44382, ...}
-        self.Schedule = {}
-        weekdays = list(calendar.day_name)
-
-        for key, value in d.items():
-            if key not in weekdays:
-                setattr(self, key, value)
-            else:
-                self.Schedule.update({key: value})
-
-    def full_name(self):
-        return self.FirstName + " " + self.LastName
-
-    def is_open(self, datetime):
-        day = datetime.strftime("%A")
-        time = int(datetime.strftime("%H"))
-
-        # Check if recipient is open for the hour after pickup
-        # Magic number 8 is the hours offset for the bitpacking scheme
-        if self.Schedule[day] & pow(2, time - 8) == 0:
-            return True
-        else:
-            return False
 
 # Read csv filenames from user
 def get_arguments():
